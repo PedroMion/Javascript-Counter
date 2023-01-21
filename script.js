a93c73d8b38e22cb567c2eff37d208e55dba4897
PedroMion <pedrombc@dcc.ufrj.br>
2023-01-21 16:50:32 -0300

    Finishing the Javascript

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
-var timestamp_actual = new Date().getTime();
 var timestamp_2024 = new Date('January 1, 2024 00:00:00 GMT+00:00').getTime();
 
-var distance_2024 = timestamp_2024 - timestamp_actual;
+const daysButton = document.querySelector("#day");
+const hoursButton = document.querySelector('#hour');
+const minutesButton = document.querySelector('#minute');
+const secondsButton = document.querySelector('#second');
 
-var seconds = distance_2024/1000;
-var minutes = seconds/60;
-var hours = minutes/60;
-var days = hours/24;
+const bottomTexts = document.querySelectorAll('.text');
 
-console.log(days);
+const titleText = document.querySelector('#title');
+
+const newYear = () => {
+    daysButton.textContent = "2";
+    hoursButton.textContent = "0";
+    minutesButton.textContent = "2";
+    secondsButton.textContent = "4";
+    titleText.textContent = "2024 is already here, happy new year!"
+    bottomTexts.forEach(element => element.textContent = "");
+}
+
+const getTime = () => {
+    var timestamp_actual = new Date().getTime();
+    var distance_2024 = timestamp_2024 - timestamp_actual;
+
+    var seconds = distance_2024/1000;
+    var minutes = seconds/60;
+    var hours = minutes/60;
+    var days = hours/24;
+    
+    var remainingDays = Math.trunc(days);
+    var remainingHours = Math.trunc(hours%24);
+    var remainingMinutes = Math.trunc(minutes%60);
+    var remainingSeconds = Math.trunc(seconds%60);
+
+    daysButton.textContent = remainingDays;
+    hoursButton.textContent = remainingHours;
+    minutesButton.textContent = remainingMinutes;
+    secondsButton.textContent = remainingSeconds;
+
+    if(timestamp_actual >= timestamp_2024) {
+        clearInterval(intervalId);
+        newYear();
+    }
+}
+
+var intervalId = window.setInterval(function(){
+    getTime()
+  }, 1000);
